5d94506496ea7f0ed6063f0c7a06ec64c181afe4
OtnielvisionereAdi2 <103239952+OtnielvisionereAdi2@users.noreply.github.com>
2022-04-08 15:23:04 +0700

    Update index.js

--- index.js
+++ index.js
@@ -46,11 +46,11 @@ function task(i) {
 	
   setTimeout(function() { 
     if(first == true){
-	bot.chat("Selamat bergabung di server ini")
+	bot.chat(".")
 	first = false;
 	}
 	else{
-		bot.chat("Diperbarui oleh vanevgt")
+		bot.chat(".")
 		first = true;
 	}
   }, 3600000 * i); 
